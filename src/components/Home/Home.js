1aa88db5a03bc4a7d79a4aad9915af875f62d6c2
JeevaRamanathan <jeevaram2000@gmail.com>
2022-03-06 10:55:35 +0530

    Nav Complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -31,12 +31,12 @@ export default class Home extends React.Component {
        
         
         <div style={{  height: "100vh" }}></div>
-        <div class="card">
-  <img class="card-img-top" src="..." alt="Card image cap"/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div className="card">
+  <img className="card-img-top" src="..." alt="Card image cap"/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
       </React.Fragment>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,365 +1,88 @@
-// import React from "react";
 import { Link } from "react-router-dom";
-// import "./navbar.css";
-
-// export default className NavBar extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       x: 0,
-//     };
-//   }
-
-//   componentDidMount() {}
-//   render() {
-//     return (
-//       <div classNameName="row">
-//         {/* <nav classNameName="navbar navbarCustom navbar-expand-lg navbar-dark bg-dark fixed-top stroke">
-//           <div classNameName="container fluid">
-//             <nav classNameName="nav-brand">
-//               <img
-//                 src="logo.png"
-//                 width="58"
-//                 height="55"
-//                 classNameName="d-inline-block align-top"
-//                 alt="Logo"
-//               />
-//             </nav>
-//             <button
-//               classNameName="navbar-toggler"
-//               type="button"
-//               data-bs-toggle="collapse"
-//               data-bs-target="#navbarText"
-//               aria-controls="navbarText"
-//               aria-expanded="false"
-//               aria-label="Toggle navigation"
-//             >
-//               <span classNameName="navbar-toggler-icon"></span>
-//             </button>
-
-//             <div
-//               classNameName="collapse navbar-collapse justify-content-end"
-//               id="navbarText"
-//             >
-//               <ul classNameName="navbar-nav">
-//                 <li classNameName="nav-item">
-//                   <Link
-//                     to="/"
-//                     classNameName="nav-link"
-//                     href="#"
-//                     data-toggle="collapse"
-//                     data-target="navbarText"
-//                   >
-//                     <span classNameName="text hover-underline-animation">
-//                       {" "}
-//                       Home{" "}
-//                     </span>
-//                   </Link>
-//                 </li>
-//                 <li classNameName="nav-item">
-//                   <Link
-//                     to="/about"
-//                     classNameName="nav-link"
-//                     // href="#"
-//                     data-toggle="collapse"
-//                     data-target="navbarText"
-//                   >
-//                     <span classNameName="text hover-underline-animation">
-//                       {" "}
-//                       About Us
-//                     </span>
-//                   </Link>
-//                 </li>
-//                 <li classNameName="nav-item">
-//                   <Link
-//                     to="/about"
-//                     classNameName="nav-link"
-//                     // href="#"
-//                     data-toggle="collapse"
-//                     data-target="navbarText"
-//                   >
-//                     <span classNameName="text hover-underline-animation">
-//                       {" "}
-//                       Contact
-//                     </span>
-//                   </Link>
-//                 </li>
-//               </ul>
-//             </div>
-//           </div>
-//         </nav> */}
-//       </div>
-//     );
-//   }
-// }
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import "./navbar.css";
-
-// export default className NavBar extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       x: 0,
-//     };
-//   }
-
-//   componentDidMount() {}
-//   render() {
-//     return (
-//       <div classNameName="row">
-//  <nav classNameName="navbar navbarCustom navbar-expand-lg navbar-dark bg-dark fixed-top stroke">
-//   <div classNameName="container fluid">
-//     <nav classNameName="nav-brand">
-//       <img
-//         src="logo.png"
-//         width="58"
-//         height="55"
-//         classNameName="d-inline-block align-top"
-//         alt="Logo"
-//       />
-//     </nav>
-//     <button
-//       classNameName="navbar-toggler"
-//       type="button"
-//       data-bs-toggle="collapse"
-//       data-bs-target="#navbarText"
-//       aria-controls="navbarText"
-//       aria-expanded="false"
-//       aria-label="Toggle navigation"
-//     >
-//       <span classNameName="navbar-toggler-icon"></span>
-//     </button>
-
-//     <div
-//       classNameName="collapse navbar-collapse justify-content-end"
-//       id="navbarText"
-//     >
-//       <ul classNameName="navbar-nav">
-//         <li classNameName="nav-item">
-//           <Link
-//             to="/"
-//             classNameName="nav-link"
-//             href="#"
-//             data-toggle="collapse"
-//             data-target="navbarText"
-//           >
-//             <span classNameName="text hover-underline-animation">
-//               {" "}
-//               Home{" "}
-//             </span>
-//           </Link>
-//         </li>
-//         <li classNameName="nav-item">
-//           <Link
-//             to="/about"
-//             classNameName="nav-link"
-//             // href="#"
-//             data-toggle="collapse"
-//             data-target="navbarText"
-//           >
-//             <span classNameName="text hover-underline-animation">
-//               {" "}
-//               About Us
-//             </span>
-//           </Link>
-//         </li>
-//         <li classNameName="nav-item">
-//           <Link
-//             to="/about"
-//             classNameName="nav-link"
-//             // href="#"
-//             data-toggle="collapse"
-//             data-target="navbarText"
-//           >
-//             <span classNameName="text hover-underline-animation">
-//               {" "}
-//               Contact
-//             </span>
-//           </Link>
-//         </li>
-//       </ul>
-//     </div>
-//   </div>
-// </nav>
-//       </div>
-//     );
-//   }
-// }
-
 import React, { useState, Fragment, useEffect } from "react";
 import "./navbar.css";
 import { FaBars } from "react-icons/fa";
-import {
-  Nav,
-  NavContainer,
-  NavLogo,
-  NavItem,
-  NavLinks,
-  NavMenu,
-  MobileIcon,
-} from "./NavbarStyles";
 const NavBar = () => {
   const [colorChange, setColorchange] = useState(false);
   const [toggle, setToggle] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [value,setValue]=useState('home')
   const changeNavbarColor = () => {
-    if (window.scrollY >= 25) {
+    if (window.scrollY >= 50) {
       setColorchange(true);
-    }
-    else if(window.scrollY <100 && toggle){
-      setColorchange(true)
-    }
-    else {
+    } else if (toggle) {
+      setColorchange(true);
+    } else {
       setColorchange(false);
     }
   };
-  const changeNavbarResize =()=>{
-    
-  }
-  window.addEventListener("scroll", changeNavbarColor);
-  // window.addEventListener("resize",changeNavbarResize)
 
-  useEffect(() => console.log(toggle))
-  const navColor = () => {
-    setToggle(!toggle)
-    if (document.getElementById("navBar").className.includes("colorChange") && window.scrollY<10) {
-      document.getElementById("navBar").className =
-        "navbar navbar-expand-lg fixed-top navbar-dark transition";
-    }
-    else if(window.scrollY>10 && document.getElementById("navBar").className.includes("colorChange")){
-      document.getElementById("navBar").className =
-      "navbar navbar-expand-lg fixed-top navbar-light colorChange";
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavbarColor)
+  })
+ 
+  const closeNav= (value)=>{
+    setValue(value)
+    document.getElementById("navbarSupportedContent").className='collapse navbar-collapse justify-content-end transition'
   }
-    
-    else {
-      document.getElementById("navBar").className =
-        "navbar navbar-expand-lg fixed-top navbar-light colorChange";
-    }
-  };
+  useEffect(() => {
+    window.addEventListener("resize",()=>setWidth(window.innerWidth));
+  });
   return (
     <Fragment>
-      {/* <nav classNameName="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div classNameName="container">
-          {" "}
-          <nav classNameName="nav-brand">
-            <img
-              src="logo.png"
-              width="58"
-              height="55"
-              classNameName="d-inline-block align-top"
-              alt="Logo"
-            />
-          </nav>
-          <button
-            classNameName="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarText"
-            aria-controls="navbarText"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span classNameName="navbar-toggler-icon"></span>
-          </button>
-          <div
-            classNameName="collapse navbar-collapse justify-content-end"
-            id="navbarText"
-          >
-            <ul classNameName="navbar-nav">
-              <li classNameName="nav-item">
-                <Link
-                  to="/"
-                  classNameName="nav-link"
-                  href="#"
-                  data-toggle="collapse"
-                  data-target="navbarText"
-                >
-                  <span classNameName="text hover-underline-animation">
-                    {" "}
-                    Home{" "}
-                  </span>
-                </Link>
-              </li>
-              <li classNameName="nav-item">
-                <Link
-                  to="/about"
-                  classNameName="nav-link"
-                  // href="#"
-                  data-toggle="collapse"
-                  data-target="navbarText"
-                >
-                  <span classNameName="text hover-underline-animation">
-                    {" "}
-                    About Us
-                  </span>
-                </Link>
-              </li>
-              <li classNameName="nav-item">
-                <Link
-                  to="/about"
-                  classNameName="nav-link"
-                  // href="#"
-                  data-toggle="collapse"
-                  data-target="navbarText"
-                >
-                  <span classNameName="text hover-underline-animation">
-                    {" "}
-                    Contact
-                  </span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav> */}
       <nav
         id="navBar"
         className={
-          colorChange
-            ? "navbar navbar-expand-lg fixed-top navbar-light  colorChange"
-            : "navbar navbar-expand-lg fixed-top navbar-dark"
+          colorChange || width < 992
+            ? "navbar navbar-expand-lg fixed-top navbar-light colorChange"
+            : "navbar navbar-expand-lg fixed-top navbar-dark transition"
         }
       >
         <div className="container">
           <a className="navbar-brand" href="#">
-          <img
-              src="logo.png"
-              width="65"
-              height="60"
-              classNameName="d-inline-block align-top"
+            <img
+              src="0logo.png"
+              width="55"
+              height="50"
+              className="d-inline-block align-top"
               alt="Logo"
             />
           </a>
-          <button
+          <div
             className="navbar-toggler"
             type="button"
-            onClick={navColor}
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+            <FaBars color="#ff7000" />
+          </div>
 
-          <div className="collapse navbar-collapse  justify-content-end " id="navbarSupportedContent">
+          <div
+            className={"collapse navbar-collapse justify-content-end"}
+            id="navbarSupportedContent"
+          >
             <ul className="navbar-nav mr-auto">
-              <li className="hover-underline-animation nav-item active">
-                <span className="nav-link" href="#">
-                  Home 
+              <li key='home' className={`nav-item hover-underline-animation ${value=='home'? "hover-underline-animation1":""}`}>
+                <span
+                  className="nav-link"
+                  onClick={()=>closeNav('home')}
+                  style={{color: colorChange || width < 992?'#343635':'white'}}
+                >
+                  Home
                 </span>
               </li>
-              <li className="nav-item">
-                <span className="nav-link" href="#">
+              <li key='about'  className={`nav-item hover-underline-animation ${value=='about'? "hover-underline-animation1":""}`}>
+                <span className="nav-link"onClick={()=>closeNav('about')}  style={{color: colorChange || width < 992?'#343635':'white'}}>
                   About
                 </span>
               </li>
             </ul>
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <span className="nav-link" href="#">
+              <li key='contact' className={`nav-item hover-underline-animation ${value=='contact'? "hover-underline-animation1":""}`}>
+                <span className="nav-link" onClick={()=>closeNav('contact')}  style={{color: colorChange || width < 992?'#343635':'white'}}>
                   Contact
                 </span>
               </li>
